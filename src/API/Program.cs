278b7d4ce97a0052639455f676a6eb632fbe4a7b
bbr6 <bbr@firtal.com>
2024-03-01 16:07:34 +0100

    Set up API to use GraphQL Set up API to use GraphiQL Set up API to use WebSockets Adding services

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,17 @@
+using MindworkingTest.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services
+    .AddExternalServices()
+    .AddServices(configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseWebSockets();
+app.UseGraphQL();
+app.UseGraphQLGraphiQL();
 
 app.Run();

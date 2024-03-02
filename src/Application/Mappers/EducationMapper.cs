585d63861581626243ac51458dc5f4a85687f2c2
bbr6 <bbr@firtal.com>
2024-03-02 22:53:16 +0100

    Added id map for education

--- src/Application/Mappers/EducationMapper.cs
+++ src/Application/Mappers/EducationMapper.cs
@@ -9,6 +9,7 @@ public static class EducationMapper
     public static Education Map(EducationTable table)
     => new()
     {
+        Id = table.Id,
         Name = table.Name,
         FieldOfStudy = table.FieldOfStudy,
         StartDate = DateTime.Parse(table.StartDate),

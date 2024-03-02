77a032dea2a736f6505044b31cfb8a94c9461551
bbr6 <bbr@firtal.com>
2024-03-02 19:33:15 +0100

    Added company mutation to mutation

--- src/Application/GraphQL/Mutation.cs
+++ src/Application/GraphQL/Mutation.cs
@@ -1,4 +1,5 @@
 using GraphQL.Types;
+using MindworkingTest.Application.Features.Companies;
 using MindworkingTest.Application.Features.Projects;
 using MindworkingTest.Application.Features.Technologies;
 
@@ -10,5 +11,6 @@ public sealed class Mutation : ObjectGraphType
     {
         Field<TechnologyMutation>("technology").Resolve(context => new { });
         Field<ProjectMutation>("project").Resolve(context => new { });
+        Field<CompanyMutation>("company").Resolve(context => new { });
     }
 }

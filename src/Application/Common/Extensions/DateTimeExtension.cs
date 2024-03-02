7347113607cd7e8d875415acea8a0d63beb9b204
bbr6 <bbr@firtal.com>
2024-03-02 15:31:25 +0100

    Added extension for string and datetime

---
+++ src/Application/Common/Extensions/DateTimeExtension.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace MindworkingTest.Application.Common.Extensions;
+
+public static class DateTimeExtension
+{
+    /// <summary>
+    /// Determines if <see cref="DateTime"/> has value then converts to string
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns>
+    /// String when <see cref="DateTime"/> has value
+    /// <para>
+    /// Null when <see cref="DateTime"/> does not have value
+    /// </para>
+    /// </returns>
+    public static string? ToStringOrNull(
+        this DateTime? dateTime,
+        [StringSyntax("DateTimeFormat")] string format)
+    {
+        if (!dateTime.HasValue)
+            return null;
+
+        return dateTime.Value.ToString(format);
+    }
+}
---
+++ src/Application/Common/Extensions/StringExtension.cs
@@ -0,0 +1,40 @@
+namespace MindworkingTest.Application.Common.Extensions;
+
+public static class StringExtension
+{
+    /// <summary>
+    /// Parses date when string has value
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns>
+    /// <see cref="DateTime" /> when valid string
+    /// <para>
+    /// Null when whitespace
+    /// </para>
+    /// </returns>
+    /// <exception cref="FormatException"/>
+    public static DateTime? ParseDateOrNull(this string? date)
+    {
+        if (string.IsNullOrWhiteSpace(date))
+            return null;
+
+        return DateTime.Parse(date);
+    }
+    /// <summary>
+    /// Check string for whitespace or null
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns>
+    /// String value when not whitespace
+    /// <para>
+    /// Null when only whitespace
+    /// </para>
+    /// </returns>
+    public static string? ValueOrNull(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return null;
+
+        return str;
+    }
+}

e00400db78e9d9df6171bc4ea009cd79549dc8ee
bbr6 <bbr@firtal.com>
2024-03-02 22:27:02 +0100

    Added educations to CV

--- src/Domain/Models/CurriculumVitae.cs
+++ src/Domain/Models/CurriculumVitae.cs
@@ -5,4 +5,5 @@ public sealed class CurriculumVitae
     public required IEnumerable<Skill> Skills { get; set; }
     public required IEnumerable<Company> Companies { get; set; }
     public required IEnumerable<Project> Projects { get; set; }
+    public required IEnumerable<Education> Educations { get; set; }
 }

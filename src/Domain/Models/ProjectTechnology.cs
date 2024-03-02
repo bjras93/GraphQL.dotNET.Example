e63d62b6fad63fe36a6f0c2f0770cb04beeb795f
bbr6 <bbr@firtal.com>
2024-03-02 16:07:30 +0100

    Updated project technology model

--- src/Domain/Models/ProjectTechnology.cs
+++ src/Domain/Models/ProjectTechnology.cs
@@ -1,12 +1,8 @@
-using MindworkingTest.Domain.Enums;
-
 namespace MindworkingTest.Domain.Models;
 
 public sealed class ProjectTechnology
 {
-    public required int Id { get; set; }
-    public required string Name { get; set; }
-    public required ProficiencyLevels ProficiencyLevel { get; set; }
-    public required int ProjectId { get; set; }
-    public required int TechnologyId { get; set; }
+    public int Id { get; set; }
+    public int ProjectId { get; set; }
+    public int TechnologyId { get; set; }
 }

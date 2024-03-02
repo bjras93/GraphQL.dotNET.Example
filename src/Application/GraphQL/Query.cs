e02836892092126e0cfbdbaf7182ebba8c926867
bbr6 <bbr@firtal.com>
2024-03-02 22:24:15 +0100

    Added descriptions for query

--- src/Application/GraphQL/Query.cs
+++ src/Application/GraphQL/Query.cs
@@ -1,6 +1,7 @@
 using GraphQL.Types;
 using MindworkingTest.Application.Features.Companies;
-using MindworkingTest.Application.Features.CurriculumVitaes;
+using MindworkingTest.Application.Features.CurriculumVitarum;
+using MindworkingTest.Application.Features.Educations;
 using MindworkingTest.Application.Features.Projects;
 using MindworkingTest.Application.Features.Technologies;
 
@@ -12,9 +13,20 @@ public sealed class Query : ObjectGraphType
     {
         Name = "Query";
         Description = "Queries for Curriculum Vitae data";
-        Field<TechnologyQuery>("Technologies").Resolve(context => new { });
-        Field<ProjectQuery>("Projects").Resolve(context => new { });
-        Field<CompanyQuery>("Companies").Resolve(context => new { });
-        Field<CurriculumVitaeQuery>("CurriculaVitarum").Resolve(context => new { });
+        Field<TechnologyQuery>("Technologies")
+            .Description("Queries for technologies")
+            .Resolve(context => new { });
+        Field<ProjectQuery>("Projects")
+            .Description("Queries for projects")
+            .Resolve(context => new { });
+        Field<CompanyQuery>("Companies")
+            .Description("Queries for companies")
+            .Resolve(context => new { });
+        Field<CurriculumVitaeQuery>("CurriculumVitarum")
+            .Description("Queries for curriculum vitarum")
+            .Resolve(context => new { });
+        Field<EducationQuery>("Educations")
+            .Description("Queries for educations")
+            .Resolve(context => new { });
     }
 }
